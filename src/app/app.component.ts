12ab86856c91311e4deb0b374dbe4fd4e34c8c47
Jeremy Cannell <jeremycannell@Jeremys-MacBook-Pro-2.local>
2016-12-01 19:09:18 -0500

    Installed Firebase SDK with npm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {initializeApp, database} from 'firebase';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello World Angular 2!';
+
+  constructor() {
+
+      var config = {
+    apiKey: "AIzaSyCIA-TBV9a8RvaRgHkfjL9BgumtBleqN5Q",
+    authDomain: "fir-project-e4c92.firebaseapp.com",
+    databaseURL: "https://fir-project-e4c92.firebaseio.com",
+    storageBucket: "fir-project-e4c92.appspot.com",
+    messagingSenderId: "852769832406"
+  };
+
+  initializeApp(config);
+
+  var root = database().ref('messages/2');
+
+  root.on('value', function(snap){
+
+  console.log(snap.key,snap.val());
+
+  });
+
+  }
 }
