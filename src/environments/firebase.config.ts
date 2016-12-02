9ba7789b834280170d6e70c4b34f2ce000c33f9d
Jeremy Cannell <jeremycannell@Jeremys-MacBook-Pro-2.local>
2016-12-01 20:05:42 -0500

    Loading data into firebase with angularefire2

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,16 @@
+import {AuthMethods, AuthProviders} from "angularfire2";
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCIA-TBV9a8RvaRgHkfjL9BgumtBleqN5Q",
+    authDomain: "fir-project-e4c92.firebaseapp.com",
+    databaseURL: "https://fir-project-e4c92.firebaseio.com",
+    storageBucket: "fir-project-e4c92.appspot.com",
+    messagingSenderId: "852769832406"
+  };
+
+
+/*
+export const authConfig = {
+    provider: AuthProviders.Password,
+    method: AuthMethods.Password
+};*/
